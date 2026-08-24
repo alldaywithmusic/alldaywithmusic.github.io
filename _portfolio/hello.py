e211e750758e948231588ebaf45d00f6ff6e5273
PRAEWPAN PROMSURIN <alldaywithmusic@gmail.com>
2026-08-24 22:04:04 +0700

    Update hello.py

--- _portfolio/hello.py
+++ _portfolio/hello.py
@@ -1,5 +1,5 @@
 ---
-title: "Portfolio item number 2"
+title: "Hello World"
 excerpt: "Short description of portfolio item number 2 <br/><img src='/images/Screenshot 2026-08-24 215828.png'>"
 collection: portfolio
 ---
